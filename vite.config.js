d9b06f7f3f4c2207578874a1c611b5bb8a7c6491
kebleInsyt <kelechichiemeka@yahoo.com>
2024-11-09 16:52:16 +0100

    changed assets folder in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
                 'resources/js/app.js'
             ]
         },
-        assetsDir: '',
+        assetsDir: 'assets',
         emptyOutDir: true,
     },
     server: {
