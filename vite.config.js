867ba8725bb4420b7beee65a461f4c1c00d50b72
kebleInsyt <kelechichiemeka@yahoo.com>
2024-11-12 02:21:04 +0100

    last update to the docker file

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,36 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
-            buildDirectory: 'build'
+            buildDirectory: "build",
         }),
     ],
     build: {
         manifest: true,
-        outDir: 'public/build',
+        outDir: "public/build",
         rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ]
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            output: {
+                assetFileNames: "assets/[name]-[hash][extname]",
+                chunkFileNames: "assets/[name]-[hash].js",
+                entryFileNames: "assets/[name]-[hash].js",
+            },
         },
-        assetsDir: 'assets',
+        assetsDir: "assets",
         emptyOutDir: true,
     },
     server: {
         hmr: {
-            host: 'localhost'
-        }
+            host: "localhost",
+        },
     },
     resolve: {
         alias: {
-            '@': '/resources/js'
-        }
-    }
+            "@": "/resources/js",
+        },
+    },
 });
